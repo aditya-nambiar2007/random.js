d54624db0c4d333c52faec3310b03d796c756395
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2023-03-22 14:32:58 +0530

    Add files via upload

--- random.js
+++ random.js
@@ -12,9 +12,9 @@ const random = {
     float:function (a, b) { return (Math.random() * (b - a + 1)) + a },
     array: {
         sort: (array) => { return array.slice().sort(() => 0.5 - Math.random()) },
-        getData: (arr, n=1) => { return random.array.sort(arr).slice(0, n) }
+        sample: (arr, n=1) => { return random.array.sort(arr).slice(0, n) }
     },
-    object:(obj,n=1)=>{
+    object_sample:(obj,n=1)=>{
         let x=Object.keys(obj).sort(() => 0.5 - Math.random()).splice(0,n)
         let ret={}
         x.forEach(e=>ret[e]=obj[e])
@@ -23,3 +23,5 @@ const random = {
     bool : ()=>{return 0.5>Math.random()}
 
 }
+
+try {module.exports = random} catch (e) {}
